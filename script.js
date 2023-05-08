2c108ff896aa75d6adbed25b3beef83a34ec9377
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-05-08 16:08:45 +0200

    Correction : Utilisation d'un scroll value en %

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 // On ne pointera pas les éléments du HTML,
 // on utilisera directement leur id.
 
-// Changer l'épaisseur de la navbar au scroll :
+// Pour afficher la popup une fois :
+let playOnce = true;
+
+// Evenement au scroll :
 window.addEventListener("scroll", () => {
+  // Navbar :
   if (window.scrollY > 0) {
     navbar.style.height = "40px";
   } else {
     navbar.style.height = "90px";
   }
-});
 
-// Faire apparaitre l'image de la partie improvise :
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
+  // On garde la valeur du scroll en % :
+  let scrollValue =
+    (window.innerHeight + window.scrollY) / document.body.offsetHeight;
+  //
+  console.log(scrollValue);
+
+  // Image :
+  if (scrollValue > 0.45) {
     imgImprovise.style.opacity = 1;
     imgImprovise.style.transform = "translateX(0px)";
   }
-});
 
-// Faire apparaitre la popup quand on est en bas du site :
-window.addEventListener("scroll", () => {
-  // Si on scroll jusqu'en bas de la page :
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+  // Popup :
+  if (scrollValue > 0.85 && playOnce) {
     popup.style.opacity = 1;
     popup.style.transform = "translateX(0px)";
+    playOnce = false;
   }
 });
 
-// Bonus : quand on clicke sur la popup elle disparait pour toujours :
+// Bonus : au click sur la popup elle disparait pour toujours :
 closeBtn.addEventListener("click", () => {
-  popup.style.display = "none";
+  popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)";
 });
